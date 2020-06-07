31ded87f7502b4a7800806da933be747a9867bc5
RMottram <16639499@students.lincoln.ac.uk>
2020-06-07 14:50:35 +0100

    Created button to switch coloured waves
    Animation does not work when done so

--- Umbriel/ContentView.swift
+++ Umbriel/ContentView.swift
@@ -53,16 +53,17 @@ struct ContentView: View
             Divider()
                 .padding(.horizontal, 20)
             
-//            Button(action:
-//                {
-//
-//            })
-//            {
-//                Text("Test Password")
-//            }.padding().font(.system(size: 20, design: .rounded))
+            Button(action: {
+                self.standby.toggle()
+                self.weak.toggle()
+                })
+            {
+                Text("Test Password")
+                 .font(.system(size: 30, design: .rounded))
+            }
+            
             
             Text("\(OneDecimal(number: score))").font(.system(size: 30, design: .rounded))
-
             
             ZStack
             {
@@ -71,24 +72,24 @@ struct ContentView: View
                 {
                     // background wave
                     Wave(interval: universalSize.width, amplitude:100, baseline: universalSize.height/6)
-                            .foregroundColor(Color.init(red: 135/255, green: 140/255, blue: 140/255, opacity: 0.4))
-                            .offset(x: isAnimated ? -1 * universalSize.width : 0)
-                            .animation(Animation.linear(duration: 6)
-                            .repeatForever(autoreverses: false))
+                        .foregroundColor(Color.init(red: 135/255, green: 140/255, blue: 140/255, opacity: 0.4))
+                        .offset(x: isAnimated ? -1 * universalSize.width : 0)
+                        .animation(Animation.linear(duration: 6)
+                        .repeatForever(autoreverses: false))
                         
-                        // midground wave
-                        Wave(interval: universalSize.width * 2, amplitude: 80, baseline: universalSize.height/6)
-                            .foregroundColor(Color.init(red: 135/255, green: 140/255, blue: 140/255, opacity: 0.6))
-                            .offset(x: isAnimated ? -1 * (universalSize.width * 2) : 0)
-                            .animation(Animation.linear(duration: 4)
-                            .repeatForever(autoreverses: false))
+                    // midground wave
+                    Wave(interval: universalSize.width * 2, amplitude: 80, baseline: universalSize.height/6)
+                        .foregroundColor(Color.init(red: 135/255, green: 140/255, blue: 140/255, opacity: 0.6))
+                        .offset(x: isAnimated ? -1 * (universalSize.width * 2) : 0)
+                        .animation(Animation.linear(duration: 4)
+                        .repeatForever(autoreverses: false))
                         
-                        //foreground
-                        Wave(interval: universalSize.width * 4, amplitude: 60, baseline: 60 + universalSize.height/6)
-                            .foregroundColor(Color.init(red: 130/255, green: 140/255, blue: 140/255, opacity: 1))
-                            .offset(x: isAnimated ? -1 * (universalSize.width * 4) : 0)
-                            .animation(Animation.linear(duration: 4)
-                            .repeatForever(autoreverses: false))
+                    //foreground
+                    Wave(interval: universalSize.width * 4, amplitude: 60, baseline: 60 + universalSize.height/6)
+                        .foregroundColor(Color.init(red: 130/255, green: 140/255, blue: 140/255, opacity: 1))
+                        .offset(x: isAnimated ? -1 * (universalSize.width * 4) : 0)
+                        .animation(Animation.linear(duration: 4)
+                        .repeatForever(autoreverses: false))
                 }
                 
                 // weak password
@@ -96,99 +97,99 @@ struct ContentView: View
                 {
                     // background wave
                     Wave(interval: universalSize.width, amplitude:100, baseline: universalSize.height/6)
-                            .foregroundColor(Color.init(red: 253/255, green: 169/255, blue: 169/255, opacity: 0.4))
-                            .offset(x: isAnimated ? -1 * universalSize.width : 0)
-                            .animation(Animation.linear(duration: 6)
-                            .repeatForever(autoreverses: false))
+                        .foregroundColor(Color.init(red: 253/255, green: 169/255, blue: 169/255, opacity: 0.4))
+                        .offset(x: isAnimated ? -1 * universalSize.width : 0)
+                        .animation(Animation.linear(duration: 6)
+                        .repeatForever(autoreverses: false))
+                    
+                    // midground wave
+                    Wave(interval: universalSize.width * 2, amplitude: 80, baseline: universalSize.height/6)
+                        .foregroundColor(Color.init(red: 255/255, green: 101/255, blue: 101/255, opacity: 0.6))
+                        .offset(x: isAnimated ? -1 * (universalSize.width * 2) : 0)
+                        .animation(Animation.linear(duration: 4)
+                        .repeatForever(autoreverses: false))
                         
-                        // midground wave
-                        Wave(interval: universalSize.width * 2, amplitude: 80, baseline: universalSize.height/6)
-                            .foregroundColor(Color.init(red: 255/255, green: 101/255, blue: 101/255, opacity: 0.6))
-                            .offset(x: isAnimated ? -1 * (universalSize.width * 2) : 0)
-                            .animation(Animation.linear(duration: 4)
-                            .repeatForever(autoreverses: false))
-                        
-                        //foreground
-                        Wave(interval: universalSize.width * 4, amplitude: 60, baseline: 60 + universalSize.height/6)
-                            .foregroundColor(Color.init(red: 255/255, green: 101/255, blue: 101/255, opacity: 1))
-                            .offset(x: isAnimated ? -1 * (universalSize.width * 4) : 0)
-                            .animation(Animation.linear(duration: 4)
-                            .repeatForever(autoreverses: false))
+                    //foreground
+                    Wave(interval: universalSize.width * 4, amplitude: 60, baseline: 60 + universalSize.height/6)
+                        .foregroundColor(Color.init(red: 255/255, green: 101/255, blue: 101/255, opacity: 1))
+                        .offset(x: isAnimated ? -1 * (universalSize.width * 4) : 0)
+                        .animation(Animation.linear(duration: 4)
+                        .repeatForever(autoreverses: false))
                 }
                 
                 // average password
                 if average
                 {
                     // background wave
-                        Wave(interval: universalSize.width, amplitude:100, baseline: universalSize.height/4)
-                            .foregroundColor(Color.init(red: 253/255, green: 205/255, blue: 169/255, opacity: 0.4))
-                            .offset(x: isAnimated ? -1 * universalSize.width : 0)
-                            .animation(Animation.linear(duration: 6)
-                            .repeatForever(autoreverses: false))
+                    Wave(interval: universalSize.width, amplitude:100, baseline: universalSize.height/4)
+                        .foregroundColor(Color.init(red: 253/255, green: 205/255, blue: 169/255, opacity: 0.4))
+                        .offset(x: isAnimated ? -1 * universalSize.width : 0)
+                        .animation(Animation.linear(duration: 6)
+                        .repeatForever(autoreverses: false))
                         
-                        // midground wave
-                        Wave(interval: universalSize.width * 2, amplitude: 80, baseline: universalSize.height/4)
-                            .foregroundColor(Color.init(red: 255/255, green: 190/255, blue: 101/255, opacity: 0.6))
-                            .offset(x: isAnimated ? -1 * (universalSize.width * 2) : 0)
-                            .animation(Animation.linear(duration: 4)
-                            .repeatForever(autoreverses: false))
+                    // midground wave
+                    Wave(interval: universalSize.width * 2, amplitude: 80, baseline: universalSize.height/4)
+                        .foregroundColor(Color.init(red: 255/255, green: 190/255, blue: 101/255, opacity: 0.6))
+                        .offset(x: isAnimated ? -1 * (universalSize.width * 2) : 0)
+                        .animation(Animation.linear(duration: 4)
+                        .repeatForever(autoreverses: false))
                         
-                        //foreground
-                        Wave(interval: universalSize.width * 4, amplitude: 60, baseline: 60 + universalSize.height/4)
-                            .foregroundColor(Color.init(red: 255/255, green: 190/255, blue: 101/255, opacity: 1))
-                            .offset(x: isAnimated ? -1 * (universalSize.width * 4) : 0)
-                            .animation(Animation.linear(duration: 4)
-                            .repeatForever(autoreverses: false))
+                    //foreground
+                    Wave(interval: universalSize.width * 4, amplitude: 60, baseline: 60 + universalSize.height/4)
+                        .foregroundColor(Color.init(red: 255/255, green: 190/255, blue: 101/255, opacity: 1))
+                        .offset(x: isAnimated ? -1 * (universalSize.width * 4) : 0)
+                        .animation(Animation.linear(duration: 4)
+                        .repeatForever(autoreverses: false))
                 }
                 
                 // strong password
                 if strong
                 {
                     // background wave
-                        Wave(interval: universalSize.width, amplitude:100, baseline: universalSize.height/4)
-                            .foregroundColor(Color.init(red: 185/255, green: 253/255, blue: 169/255, opacity: 0.4))
-                            .offset(x: isAnimated ? -1 * universalSize.width : 0)
-                            .animation(Animation.linear(duration: 6)
-                            .repeatForever(autoreverses: false))
+                    Wave(interval: universalSize.width, amplitude:100, baseline: universalSize.height/4)
+                        .foregroundColor(Color.init(red: 185/255, green: 253/255, blue: 169/255, opacity: 0.4))
+                        .offset(x: isAnimated ? -1 * universalSize.width : 0)
+                        .animation(Animation.linear(duration: 6)
+                        .repeatForever(autoreverses: false))
                         
-                        // midground wave
-                        Wave(interval: universalSize.width * 2, amplitude: 80, baseline: universalSize.height/4)
-                            .foregroundColor(Color.init(red: 117/255, green: 211/255, blue: 99/255, opacity: 0.6))
-                            .offset(x: isAnimated ? -1 * (universalSize.width * 2) : 0)
-                            .animation(Animation.linear(duration: 4)
-                            .repeatForever(autoreverses: false))
+                    // midground wave
+                    Wave(interval: universalSize.width * 2, amplitude: 80, baseline: universalSize.height/4)
+                        .foregroundColor(Color.init(red: 117/255, green: 211/255, blue: 99/255, opacity: 0.6))
+                        .offset(x: isAnimated ? -1 * (universalSize.width * 2) : 0)
+                        .animation(Animation.linear(duration: 4)
+                        .repeatForever(autoreverses: false))
                         
-                        //foreground
-                        Wave(interval: universalSize.width * 4, amplitude: 60, baseline: 60 + universalSize.height/4)
-                            .foregroundColor(Color.init(red: 117/255, green: 211/255, blue: 99/255, opacity: 1))
-                            .offset(x: isAnimated ? -1 * (universalSize.width * 4) : 0)
-                            .animation(Animation.linear(duration: 4)
-                            .repeatForever(autoreverses: false))
+                    //foreground
+                    Wave(interval: universalSize.width * 4, amplitude: 60, baseline: 60 + universalSize.height/4)
+                        .foregroundColor(Color.init(red: 117/255, green: 211/255, blue: 99/255, opacity: 1))
+                        .offset(x: isAnimated ? -1 * (universalSize.width * 4) : 0)
+                        .animation(Animation.linear(duration: 4)
+                        .repeatForever(autoreverses: false))
                 }
                 
                 // very strong password
                 if veryStrong
                 {
                     // background wave
-                        Wave(interval: universalSize.width, amplitude:100, baseline: universalSize.height/4)
-                            .foregroundColor(Color.init(red: 149/255, green: 140/255, blue: 255/255, opacity: 0.4))
-                            .offset(x: isAnimated ? -1 * universalSize.width : 0)
-                            .animation(Animation.linear(duration: 6)
-                            .repeatForever(autoreverses: false))
+                    Wave(interval: universalSize.width, amplitude:100, baseline: universalSize.height/4)
+                        .foregroundColor(Color.init(red: 149/255, green: 140/255, blue: 255/255, opacity: 0.4))
+                        .offset(x: isAnimated ? -1 * universalSize.width : 0)
+                        .animation(Animation.linear(duration: 6)
+                        .repeatForever(autoreverses: false))
                         
-                        // midground wave
-                        Wave(interval: universalSize.width * 2, amplitude: 80, baseline: universalSize.height/4)
-                            .foregroundColor(Color.init(red: 127/255, green: 73/255, blue: 255/255, opacity: 0.6))
-                            .offset(x: isAnimated ? -1 * (universalSize.width * 2) : 0)
-                            .animation(Animation.linear(duration: 4)
-                            .repeatForever(autoreverses: false))
+                    // midground wave
+                    Wave(interval: universalSize.width * 2, amplitude: 80, baseline: universalSize.height/4)
+                        .foregroundColor(Color.init(red: 127/255, green: 73/255, blue: 255/255, opacity: 0.6))
+                        .offset(x: isAnimated ? -1 * (universalSize.width * 2) : 0)
+                        .animation(Animation.linear(duration: 4)
+                        .repeatForever(autoreverses: false))
                         
-                        //foreground
-                        Wave(interval: universalSize.width * 4, amplitude: 60, baseline: 60 + universalSize.height/4)
-                            .foregroundColor(Color.init(red: 127/255, green: 73/255, blue: 255/255, opacity: 1))
-                            .offset(x: isAnimated ? -1 * (universalSize.width * 4) : 0)
-                            .animation(Animation.linear(duration: 4)
-                            .repeatForever(autoreverses: false))
+                    //foreground
+                    Wave(interval: universalSize.width * 4, amplitude: 60, baseline: 60 + universalSize.height/4)
+                        .foregroundColor(Color.init(red: 127/255, green: 73/255, blue: 255/255, opacity: 1))
+                        .offset(x: isAnimated ? -1 * (universalSize.width * 4) : 0)
+                        .animation(Animation.linear(duration: 4)
+                        .repeatForever(autoreverses: false))
                 }
                 
             }.onAppear()
@@ -197,26 +198,25 @@ struct ContentView: View
             }
         }
     }
-}
-
-func Wave(interval:CGFloat, amplitude:CGFloat = 100, baseline: CGFloat = UIScreen.main.bounds.height/2) -> Path
-{
-    //let universalSize = UIScreen.main.bounds
-        
-    Path
-        {path in
-            path.move(to: CGPoint(x: 0, y: baseline))
-            path.addCurve(
-                to: CGPoint(x: 1 * interval, y: baseline),
-                control1: CGPoint(x: interval * (0.35), y: amplitude + baseline),
-                control2: CGPoint(x: interval * (0.65), y: -amplitude + baseline))
-            path.addCurve(
-                to: CGPoint(x: 2 * interval, y: baseline),
-                control1: CGPoint(x: interval * (1.35), y: amplitude + baseline),
-                control2: CGPoint(x: interval * (1.65), y: -amplitude + baseline))
-            path.addLine(to: CGPoint(x: 2 * interval, y: UIScreen.main.bounds.height))
-            path.addLine(to: CGPoint(x: 0, y: UIScreen.main.bounds.height))
+    
+    func Wave(interval: CGFloat, amplitude: CGFloat = 100, baseline: CGFloat = UIScreen.main.bounds.height/2) -> Path
+    {
+        Path
+            { path in
+                path.move(to: CGPoint(x: 0, y: baseline))
+                path.addCurve(
+                    to: CGPoint(x: 1 * interval, y: baseline),
+                    control1: CGPoint(x: interval * (0.35), y: amplitude + baseline),
+                    control2: CGPoint(x: interval * (0.65), y: -amplitude + baseline))
+                path.addCurve(
+                    to: CGPoint(x: 2 * interval, y: baseline),
+                    control1: CGPoint(x: interval * (1.35), y: amplitude + baseline),
+                    control2: CGPoint(x: interval * (1.65), y: -amplitude + baseline))
+                path.addLine(to: CGPoint(x: 2 * interval, y: UIScreen.main.bounds.height))
+                path.addLine(to: CGPoint(x: 0, y: UIScreen.main.bounds.height))
+        }
     }
+    
 }
 
 enum PasswordScore
