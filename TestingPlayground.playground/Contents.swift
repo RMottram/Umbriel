fe3fc030988d025d1985444f4eba028194b8739b
RMottram <16639499@students.lincoln.ac.uk>
2020-06-15 15:19:27 +0100

    Created the scoring logic in Playground

--- TestingPlayground.playground/Contents.swift
+++ TestingPlayground.playground/Contents.swift
@@ -10,58 +10,118 @@ enum StrengthScore: Int
     case VeryStrong = 10
 }
 
-let mypassword = "PAAAA"
+let mypassword = "passwordBA111"
 // "^([A-Za-z0-9!@#$%^&*()\\-_=+{}|?>.<,:;~`’]){2,}$"
+// let passwordRegex = "^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z!@#$%^&*()\\-_=+{}|?>.<,:;~`’]{2,}$"
 
 // master regex
 //func isValidPassword(password: String) -> Bool
 //{
-//    let passwordRegex = "^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z!@#$%^&*()\\-_=+{}|?>.<,:;~`’]{8,}$"
+//    let passwordRegex = "^(?=.*[A-Z].*[A-Z])(?=.*[!@#$&*])(?=.*[0-9].*[0-9])(?=.*[a-z].*[a-z].*[a-z]).{8,}$"
 //    return NSPredicate(format: "SELF MATCHES %@", passwordRegex).evaluate(with: password)
 //}
 //isValidPassword(password: mypassword)
 
+
+
+
+
+
+// works
+func isMoreThanOneUpper(password: String) -> Bool
+{
+    let passwordRegex = "^(?=.*[A-Z]).{1,}$"
+    return NSPredicate(format: "SELF MATCHES %@", passwordRegex).evaluate(with: password)
+}
+isMoreThanOneUpper(password: mypassword)
+
 // works
 func isMoreThanTwoUpper(password: String) -> Bool
 {
-    let passwordRegex = "^[A-Za-z]{2,}$"
+    let passwordRegex = "^(?=.*[A-Z].*[A-Z]).{1,}$"
     return NSPredicate(format: "SELF MATCHES %@", passwordRegex).evaluate(with: password)
 }
 isMoreThanTwoUpper(password: mypassword)
 
 // works
-//func isMoreThanTwoLower(password: String) -> Bool
-//{
-//    let passwordRegex = "^([a-z]{2,})$"
-//    return NSPredicate(format: "SELF MATCHES %@", passwordRegex).evaluate(with: password)
-//}
-//isMoreThanTwoLower(password: mypassword)
+func isMoreThanThreeUpper(password: String) -> Bool
+{
+    let passwordRegex = "^(?=.*[A-Z].*[A-Z].*[A-Z]).{1,}$"
+    return NSPredicate(format: "SELF MATCHES %@", passwordRegex).evaluate(with: password)
+}
+isMoreThanThreeUpper(password: mypassword)
 
 // works
-//func isMoreThanTwoNumbers(password: String) -> Bool
-//{
-//    let passwordRegex = "^([0-9]{2,})$"
-//    return NSPredicate(format: "SELF MATCHES %@", passwordRegex).evaluate(with: password)
-//}
-//isMoreThanTwoNumbers(password: mypassword)
+func isMoreThanOneLower(password: String) -> Bool
+{
+    let passwordRegex = "^(?=.*[a-z]).{1,}$"
+    return NSPredicate(format: "SELF MATCHES %@", passwordRegex).evaluate(with: password)
+}
+isMoreThanOneLower(password: mypassword)
 
 // works
-//func isMoreThanTwoSpecial(password: String) -> Bool
-//{
-//    let passwordRegex = "^[!@#$%^&*()\\-_=+{}|?>.<,:;~`’]{2,}$"
-//    return NSPredicate(format: "SELF MATCHES %@", passwordRegex).evaluate(with: password)
-//}
-//isMoreThanTwoSpecial(password: mypassword)
+func isMoreThanTwoLower(password: String) -> Bool
+{
+    let passwordRegex = "^(?=.*[a-z].*[a-z]).{1,}$"
+    return NSPredicate(format: "SELF MATCHES %@", passwordRegex).evaluate(with: password)
+}
+isMoreThanTwoLower(password: mypassword)
 
 // works
-//func isMoreThanTwoAll(password: String) -> Bool
-//{
-//    let passwordRegex = "^([A-Za-z0-9!@#$%^&*()\\-_=+{}|?>.<,:;~`’]){2,}$"
-//    return NSPredicate(format: "SELF MATCHES %@", passwordRegex).evaluate(with: password)
-//}
-//isMoreThanTwoAll(password: mypassword)
+func isMoreThanThreeLower(password: String) -> Bool
+{
+    let passwordRegex = "^(?=.*[a-z].*[a-z].*[a-z]).{1,}$"
+    return NSPredicate(format: "SELF MATCHES %@", passwordRegex).evaluate(with: password)
+}
+isMoreThanThreeLower(password: mypassword)
 
+// works
+func isMoreThanOneNumber(password: String) -> Bool
+{
+    let passwordRegex = "^(?=.*[0-9]).{1,}$"
+    return NSPredicate(format: "SELF MATCHES %@", passwordRegex).evaluate(with: password)
+}
+isMoreThanOneNumber(password: mypassword)
 
+// works
+func isMoreThanTwoNumbers(password: String) -> Bool
+{
+    let passwordRegex = "^(?=.*[0-9].*[0-9]).{1,}$"
+    return NSPredicate(format: "SELF MATCHES %@", passwordRegex).evaluate(with: password)
+}
+isMoreThanTwoNumbers(password: mypassword)
+
+// works
+func isMoreThanThreeNumbers(password: String) -> Bool
+{
+    let passwordRegex = "^(?=.*[0-9].*[0-9].*[0-9]).{1,}$"
+    return NSPredicate(format: "SELF MATCHES %@", passwordRegex).evaluate(with: password)
+}
+isMoreThanThreeNumbers(password: mypassword)
+
+// works
+func isMoreThanOneSpecial(password: String) -> Bool
+{
+    let passwordRegex = "^(?=.*[-{};:,.<>£!@#_$&*]).{1,}$"
+    return NSPredicate(format: "SELF MATCHES %@", passwordRegex).evaluate(with: password)
+}
+isMoreThanOneSpecial(password: mypassword)
+
+// works
+func isMoreThanTwoSpecial(password: String) -> Bool
+{
+    let passwordRegex = "^(?=.*[-{};:,.<>£!@#_$&*].*[-{};:,.<>£!@#_$&*]).{1,}$"
+    return NSPredicate(format: "SELF MATCHES %@", passwordRegex).evaluate(with: password)
+}
+isMoreThanTwoSpecial(password: mypassword)
+
+// works
+func isMoreThanThreeSpecial(password: String) -> Bool
+{
+    let passwordRegex = "^(?=.*[-{};:,.<>£!@#_$&*].*[-{};:,.<>£!@#_$&*].*[-{};:,.<>£!@#_$&*]).{1,}$"
+    return NSPredicate(format: "SELF MATCHES %@", passwordRegex).evaluate(with: password)
+}
+isMoreThanTwoSpecial(password: mypassword)
 
 func TestStrength(password: String) -> Double
 {
@@ -69,34 +129,90 @@ func TestStrength(password: String) -> Double
     
     if password.count == 0 {
         score += 0
-        print("Nothing entered")
+        print("DEBUG: Nothing entered. Score = \(score)")
     }
-    if password.count > 1 {
+    if password.count >= 1 {
+        score += 0
+        print("DEBUG: longer than 1 character entered. Score =  \(score)")
+    }
+    if password.count >= 6 {
         score += 0.5
-        print("1+ entered")
+        print("DEBUG: longer than 6 characters entered. Score =  \(score)")
     }
-    if password.count > 6 {
+    if password.count >= 11 {
         score += 0.5
-        print("6+ entered")
+        print("DEBUG: longer than 11 characters entered. Score =  \(score)")
     }
-    if password.count > 11 {
-        score += 0.5
-        print("11+ entered")
+    if password.count >= 16 {
+        score += 1
+        print("DEBUG: longer than 16 characters entered. Score =  \(score)")
     }
-    if password.count > 16 {
-        score += 0.5
-        print("16+ entered")
+    if isMoreThanThreeUpper(password: password)
+    {
+        score += 2
+        print("DEBUG: at least 3 upper entered. Score =  \(score)")
     }
-    if isMoreThanTwoUpper(password: password)
+    else if isMoreThanTwoUpper(password: password)
     {
         score += 1
-        print("2 or more upper entered")
+        print("DEBUG: at least 2 upper entered. Score =  \(score)")
+    }
+    else if isMoreThanOneUpper(password: password)
+    {
+        score += 0.5
+        print("DEBUG: at least 1 upper entered. Score =  \(score)")
+    }
+    if isMoreThanThreeLower(password: password)
+    {
+        score += 2
+        print("DEBUG: at least 3 lower entered. Score =  \(score)")
+    }
+    else if isMoreThanTwoLower(password: password)
+    {
+        score += 1
+        print("DEBUG: at least 2 lower entered. Score =  \(score)")
+    }
+    else if isMoreThanOneLower(password: password)
+    {
+        score += 0.5
+        print("DEBUG: at least 1 lower entered. Score =  \(score)")
+    }
+    if isMoreThanThreeNumbers(password: password)
+    {
+        score += 2
+        print("DEBUG: at least 3 numbers entered. Score =  \(score)")
+    }
+    else if isMoreThanTwoNumbers(password: password)
+    {
+        score += 1
+        print("DEBUG: at least 2 numbers entered. Score =  \(score)")
+    }
+    else if isMoreThanOneNumber(password: password)
+    {
+        score += 0.5
+        print("DEBUG: at least 1 numbers entered. Score =  \(score)")
+    }
+    if isMoreThanThreeSpecial(password: password)
+    {
+        score += 2
+        print("DEBUG: at least 3 special entered. Score =  \(score)")
+    }
+    else if isMoreThanTwoSpecial(password: password)
+    {
+        score += 1
+        print("DEBUG: at least 2 special entered. Score =  \(score)")
+    }
+    else if isMoreThanOneSpecial(password: password)
+    {
+        score += 0.5
+        print("DEBUG: at least 1 special entered. Score =  \(score)")
     }
     
+    print("DEBUG: End of scoring, final score = \(score)")
     return score
 }
 
-TestStrength(password: "PAAAAAssword<>?,.")
+TestStrength(password: "zH3£qQP8*)(m9mB0")
 
 
 //let passwordRegex = ("^(?=.*[@#$%^&*])$")
