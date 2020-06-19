7643f01619971deb5d69d867aed07b6cffdec6ec
RMottram <16639499@students.lincoln.ac.uk>
2020-06-19 17:30:08 +0100

    Started information View

---
+++ Umbriel/InformationView.swift
@@ -0,0 +1,21 @@
+//
+//  InformationView.swift
+//  Umbriel
+//
+//  Created by Ryan Mottram on 19/06/2020.
+//  Copyright © 2020 Ryan Mottram. All rights reserved.
+//
+
+import SwiftUI
+
+struct InformationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct InformationView_Previews: PreviewProvider {
+    static var previews: some View {
+        InformationView()
+    }
+}
