b8767047919862a40d9a2e5cb295528d114d3f3b
RMottram <16639499@students.lincoln.ac.uk>
2020-06-19 21:09:06 +0100

    Adjusted clipboard button alignment and tips' font size

--- Umbriel/ContentView.swift
+++ Umbriel/ContentView.swift
@@ -56,7 +56,6 @@ struct ContentView: View
     {
         VStack
             {
-                
                 ZStack
                     {
                         HStack
@@ -89,50 +88,54 @@ struct ContentView: View
                 Divider()
                     .padding(.horizontal, 20)
                 
-                Button(action: {
-                    self.isStandby = false
-                    
-                    switch self.passwordTester.TestStrength(password: self.password)
+                HStack {
+                    Spacer()
+                    Button(action: {
+                        self.isStandby = false
+                        
+                        switch self.passwordTester.TestStrength(password: self.password)
+                        {
+                            case .Blank, .Weak:
+                                self.isWeak = true
+                                self.isAverage = false
+                                self.isStrong = false
+                                self.isVeryStrong = false
+                            case .Average:
+                                self.isWeak = false
+                                self.isAverage = true
+                                self.isStrong = false
+                                self.isVeryStrong = false
+                            case .Strong:
+                                self.isWeak = false
+                                self.isAverage = false
+                                self.isStrong = true
+                                self.isVeryStrong = false
+                            case .VeryStrong:
+                                self.isWeak = false
+                                self.isAverage = false
+                                self.isStrong = false
+                                self.isVeryStrong = true
+                        }
+                        
+                    })
                     {
-                        case .Blank, .Weak:
-                            self.isWeak = true
-                            self.isAverage = false
-                            self.isStrong = false
-                            self.isVeryStrong = false
-                        case .Average:
-                            self.isWeak = false
-                            self.isAverage = true
-                            self.isStrong = false
-                            self.isVeryStrong = false
-                        case .Strong:
-                            self.isWeak = false
-                            self.isAverage = false
-                            self.isStrong = true
-                            self.isVeryStrong = false
-                        case .VeryStrong:
-                            self.isWeak = false
-                            self.isAverage = false
-                            self.isStrong = false
-                            self.isVeryStrong = true
+                        Text("Test Password")
+                            .font(.system(size: 25, design: .rounded))
                     }
-                    
-                })
-                {
-                    Text("Test Password")
-                        .font(.system(size: 30, design: .rounded))
+                    Spacer()
+
+                    Button(action: { UIPasteboard.general.string = self.password })
+                    {
+                        Image(systemName: "doc.on.clipboard")
+                            .resizable()
+                            .frame(width: 30, height: 35)
+                            .foregroundColor(Color.init(red: 135/255, green: 140/255, blue: 140/255, opacity: 0.4))
+                    }
+                    Spacer()
                 }
                 
                 // displays the password score. not for user viewing
                 //Text("\(OneDecimal(number: score))").font(.system(size: 30, design: .rounded))
-                
-                Button(action: { UIPasteboard.general.string = self.password })
-                {
-                    Image(systemName: "doc.on.clipboard")
-                        .resizable()
-                        .frame(width: 30, height: 35)
-                        .foregroundColor(Color.init(red: 135/255, green: 140/255, blue: 140/255, opacity: 0.4))
-
-                }
                 
                 ZStack
                     {
@@ -141,27 +144,28 @@ struct ContentView: View
                             WaveView(baselineAdjustment: $baseline, amplitudeAdjustment: $amplitude, animationDuration: $animationDuration, red: $weakRed, green: $weakGreen, blue: $weakBlue, opacity: $opacity)
                             
                             Text("Password is blank or too weak. Try make your password 6 characters minimum!")
-                                .font(.system(.title, design: .rounded))
+                                .font(.system(size: 25, design: .rounded))
                                 .multilineTextAlignment(.center)
-                                .padding(.horizontal, 40)
+                                .padding(.horizontal, 20)
                         }
                         if isAverage
                         {
                             WaveView(baselineAdjustment: $baseline, amplitudeAdjustment: $amplitude, animationDuration: $animationDuration, red: $avgRed, green: $avgGreen, blue: $avgBlue, opacity: $opacity)
                             
                             Text("Your password is average, try mix upper, lower case letters, numbers and special symbols!")
-                               .font(.system(.title, design: .rounded))
+                                .font(.system(size: 25, design: .rounded))
                                 .multilineTextAlignment(.center)
-                                .padding(.horizontal, 40)
+                                .padding(.horizontal, 20)
+                            
                         }
                         if isStrong
                         {
                             WaveView(baselineAdjustment: $baseline, amplitudeAdjustment: $amplitude, animationDuration: $animationDuration, red: $strongRed, green: $strongGreen, blue: $strongBlue, opacity: $opacity)
                             
                             Text("Your password is strong but can be stronger. Try and incorporate more of what you already have done!")
-                                .font(.system(.title, design: .rounded))
+                                .font(.system(size: 25, design: .rounded))
                                 .multilineTextAlignment(.center)
-                                .padding(.horizontal, 40)
+                                .padding(.horizontal, 20)
                             
                         }
                         if isVeryStrong
@@ -169,22 +173,23 @@ struct ContentView: View
                             WaveView(baselineAdjustment: $baseline, amplitudeAdjustment: $amplitude, animationDuration: $animationDuration, red: $vstrongRed, green: $vstrongGreen, blue: $vstrongBlue, opacity: $opacity)
                             
                             Text("Well Done, This password is very strong!")
-                                .font(.system(.title, design: .rounded))
+                                .font(.system(size: 25, design: .rounded))
                                 .multilineTextAlignment(.center)
-                                .padding(.horizontal, 40)
-
+                                .padding(.horizontal, 20)
+                            
                         }
                         if isStandby
                         {
                             WaveView(baselineAdjustment: $baseline, amplitudeAdjustment: $amplitude, animationDuration: $animationDuration, red: $stbRed, green: $stbGreen, blue: $stbBlue, opacity: $opacity)
                             
                             Text("Please enter a password to test its strength")
-                                .font(.system(.title, design: .rounded))
+                                .font(.system(size: 25, design: .rounded))
                                 .multilineTextAlignment(.center)
-                                .padding(.horizontal, 40)
+                                .padding(.horizontal, 20)
                         }
-                }.padding(.top, 80)
+                }.padding(.top, 40)
         }
+        
     }
     
     enum PasswordScore
