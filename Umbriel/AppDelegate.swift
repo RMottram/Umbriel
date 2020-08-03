e0456b7698868617e70e79c6dbf01ac92b167f68
RMottram <16639499@students.lincoln.ac.uk>
2020-08-03 16:29:32 +0100

    Added iCloud sync and login item label

--- Umbriel/AppDelegate.swift
+++ Umbriel/AppDelegate.swift
@@ -35,14 +35,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     // MARK: - Core Data stack
 
-    lazy var persistentContainer: NSPersistentContainer = {
+    lazy var persistentContainer: NSPersistentCloudKitContainer = {
         /*
          The persistent container for the application. This implementation
          creates and returns a container, having loaded the store for the
          application to it. This property is optional since there are legitimate
          error conditions that could cause the creation of the store to fail.
         */
-        let container = NSPersistentContainer(name: "Umbriel")
+        let container = NSPersistentCloudKitContainer(name: "Umbriel")
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
                 // Replace this implementation with code to handle the error appropriately.
@@ -59,8 +59,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                 fatalError("Unresolved error \(error), \(error.userInfo)")
             }
         })
+        
+        container.viewContext.automaticallyMergesChangesFromParent = true
+        container.viewContext.mergePolicy = NSMergeByPropertyObjectTrumpMergePolicy
+        
         return container
     }()
+    
 
     // MARK: - Core Data Saving support
 
--- Umbriel/Views/VaultView.swift
+++ Umbriel/Views/VaultView.swift
@@ -15,12 +15,13 @@ struct VaultView: View {
     
     @FetchRequest(
         entity: Vault.entity(),
-        sortDescriptors: [NSSortDescriptor(keyPath: \Vault.dateCreated, ascending: false)]
+        sortDescriptors: [NSSortDescriptor(keyPath: \Vault.title, ascending: true)]
     ) var createdPasswords: FetchedResults<Vault>
     
     @State private var noBiometrics:Bool = false
     @State private var isUnlocked: Bool = false
     @State var passwordTitle = ""
+    @State var loginItem = ""
     @State var passwordEntry = ""
     
     var body: some View {
@@ -35,19 +36,23 @@ struct VaultView: View {
                         VStack {
                             VStack {
                                 
-                                Text("TheVault is where you can safely store any passwords you may want to keep in a safe and secure environment using Apples own security built-in to every device and iOS.").font(.system(.callout, design: .rounded)).fontWeight(.regular).padding(.bottom, 20)
+                                Text("TheVault is where you can safely store any passwords you may want to keep in a safe and secure environment using Apples own security built-in to every device and iOS.").font(.system(.footnote, design: .rounded)).fontWeight(.regular)
                                 
-                                TextField("Password Description", text: self.$passwordTitle).font(.system(.largeTitle, design: .rounded))
+                                Divider()
+                                
+                                TextField("Password Description", text: self.$passwordTitle).font(.system(.title, design: .rounded))
+                                TextField("Login Item", text: self.$loginItem).font(.system(.body, design: .rounded))
                                 TextField("Password", text: self.$passwordEntry).font(.system(.body, design: .rounded))
                                     .disableAutocorrection(true)
                                     .autocapitalization(.none)
                                 
                             }.padding(.horizontal, 20)
-                            
+                                                        
                             List {
                                 ForEach(createdPasswords){ passwords in
                                     EntryRow(passwordEntry: passwords)
                                 }.onDelete(perform: removePasswordEntry)
+                                
                             }
                         }
                             
@@ -64,8 +69,8 @@ struct VaultView: View {
             } else {
                 
                 VStack {
-                    Text("TheVault uses biometric security to keep this section safe and secure.")
-                        .multilineTextAlignment(.center).padding().font(.system(.body, design: .rounded))
+                    Text("TheVault requires the use of your devices' TouchID or FaceID sensors to be used. If these have not been activated please activate them in your devices settings.")
+                        .multilineTextAlignment(.center).padding(25).font(.system(.body, design: .rounded))
                     Button("Unlock TheVault") {
                         self.authenticate()
                     }
@@ -80,6 +85,9 @@ struct VaultView: View {
         }
         .alert(isPresented: $noBiometrics) {
             Alert(title: Text("No biometrics available"), message: Text("This device has no biometric security setup. Please setup biometrics in device settings to use TheVault."), dismissButton: .default(Text("OK")))
+        }.onReceive(NotificationCenter.default.publisher(for: UIApplication.willResignActiveNotification)) { _ in
+            
+            self.lockVault()
         }
     }
     
@@ -89,6 +97,7 @@ struct VaultView: View {
         
         newPassword.id = UUID()
         newPassword.title = passwordTitle
+        newPassword.loginItem = loginItem
         newPassword.password = passwordEntry
         newPassword.dateCreated = Date()
         
@@ -99,6 +108,7 @@ struct VaultView: View {
         }
         
         passwordTitle = ""
+        loginItem = ""
         passwordEntry = ""
         
     }
@@ -147,7 +157,8 @@ struct EntryRow: View {
         
         HStack {
             VStack(alignment: .leading) {
-                Text(passwordEntry.title ?? "No title given").font(.system(.title, design: .rounded)).bold()
+                Text(passwordEntry.title ?? "No title given").font(.system(.headline, design: .rounded)).bold()
+                Text(passwordEntry.loginItem ?? "No login item given").font(.system(.subheadline, design: .rounded))
                 if self.isHidden {
                     Text("\(passwordEntry.password!)").font(.system(.body, design: .rounded)).blur(radius: 4, opaque: false)
                 } else {
@@ -159,7 +170,7 @@ struct EntryRow: View {
             
             Button(action: { self.isHidden.toggle() })
             {
-                Image(systemName: self.isHidden ? "eye.slash.fill" : "eye.fill").resizable().frame(width: 30, height: 20)
+                Image(systemName: self.isHidden ? "eye.slash.fill" : "eye.fill")
                     .foregroundColor((self.isHidden == false ) ? Color.init(red: 117/255, green: 211/255, blue: 99/255) : (Color.init(red: 255/255, green: 101/255, blue: 101/255)))
             }
         }
